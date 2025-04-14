41a02b888c49e987c2b7243d1dd1a436d5f1a4fc
dianasim <diana.simeonova56@gmail.com>
2025-04-14 22:32:44 +0300

    stuff

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 from storage import *
 
-app = Flask("app")
+app = Flask(__name__, static_folder='../frontend', static_url_path='')
 CORS(app, 
      resources={
          r"/*": {"origins": "*",
